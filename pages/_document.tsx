2c2b2280ecf8ef17c9933fda5bafc74d463e80fe
feus <barca105@nate.com>
2022-04-18 17:36:04 +0900

    메인 화면 설정

---
+++ pages/_document.tsx
@@ -0,0 +1,14 @@
+import {Html, Main, Head, NextScript} from "next/document";
+
+export default function Document() {
+
+	return (
+		<Html>
+			<Head/>
+			<body className="bg-neutral-900 text-white">
+				<Main/>
+				<NextScript/>
+			</body>
+		</Html>
+	)
+}
