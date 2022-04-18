0a84fac67597b2f8cd5dc506cddbe4023f9a123e
feus <barca105@nate.com>
2022-04-18 17:35:26 +0900

    메인 화면 축소

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<div className="mx-2 my-2 w-96 h-96 w-full mx-auto border-white border-2 my-5 py-2">
+			<Component {...pageProps} />
+		</div>
+	)
 }
 
 export default MyApp
