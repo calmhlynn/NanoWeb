36d283f3fdf1e5d26e5b0034994e9dd0ae8950d5
feus <barca105@nate.com>
2022-04-18 18:02:22 +0900

    메인화면

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,11 @@ import type {AppProps} from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<div className="mx-2 my-2 w-96 h-96 w-full mx-auto border-white border-2 my-5 py-2">
-			<Component {...pageProps} />
-		</div>
+		<>
+			<div className="mx-2 my-2 w-96 h-96 w-full max-w-sm mx-auto border-white border-2 my-5 py-2">
+				<Component {...pageProps} />
+			</div>
+		</>
 	)
 }
 
