b75d6535ea3c06f92e5c74ac2b532345407e0109
feus <barca105@nate.com>
2022-04-19 17:53:21 +0900

    상단 홈 버튼 제작

---
+++ components/button/home.tsx
@@ -0,0 +1,31 @@
+import React, {SyntheticEvent} from "react";
+import {childProps, clickProps} from "../../libs/configType";
+
+export default function HomeButton({ parentCallback }: childProps) {
+
+
+	const homeClick = (event: SyntheticEvent<HTMLButtonElement>) => {
+		parentCallback(event.currentTarget.value as clickProps);
+	}
+
+	return (
+		<button
+			className="absolute ml-3 px-2 py-1 left-0 bg-black border-white border-2 rounded-xl w-auto my-2 hover:text-gray-400 transition duration-300"
+			onClick={homeClick}
+			value="MainView"
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				className="h-6 w-6"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+				strokeWidth={2}>
+				<path
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
+			</svg>
+		</button>
+	)
+}
